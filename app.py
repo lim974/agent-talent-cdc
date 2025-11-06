d126d2b033f7dc8e9fadb21f299644a056eddb50
lim974 <laurent_imanache@sidr.fr>
2025-11-06 21:45:47 +0400

    bug fix part 5

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 import os
